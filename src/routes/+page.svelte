5f1f4b8c8ea62dc59501b853ea2f0d4c84ce8481
i-robert2 <i-robert2@users.noreply.github.com>
2026-04-21 14:47:48 +0300

    fix: mobile horizontal overflow and remove base path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -86,7 +86,7 @@
 
 	.project-grid {
 		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(340px, 1fr));
+		grid-template-columns: repeat(auto-fill, minmax(min(100%, 340px), 1fr));
 		gap: 1.25rem;
 	}
 
